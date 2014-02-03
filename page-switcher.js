5e9769f36047dc4c696eb11a8ff3faf6060b025b
Mathieu Bigorne <mathieu.bigorne@gmail.com>
2014-02-03 13:36:54 +0100

    Naive page switcher

---
+++ page-switcher.js
@@ -0,0 +1,25 @@
+var SWITCH_DELAY = 4000;
+
+var PAGES = [
+    "barchart.html?track=Front",
+    "barchart.html?track=Divers",
+    "barchart.html?track=Craft",
+    "barchart.html?track=Data",
+    "barchart.html?track=Agile",
+    "topspeaker.html"
+];
+
+function getCurrentPageIndex() {
+    var currentLocation = window.location.toString();
+    currentLocation = currentLocation.substring(currentLocation.lastIndexOf('/') + 1);
+    var currentPageIndex = PAGES.indexOf(currentLocation);
+    return currentPageIndex;
+}
+
+if (getCurrentPageIndex() == -1) {
+    window.location = PAGES[0];
+}
+
+setTimeout(function () {
+    window.location = PAGES[(getCurrentPageIndex() + 1) % PAGES.length];
+}, SWITCH_DELAY);
