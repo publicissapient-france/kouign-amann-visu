f3d4940c6dde7e932db052c5fbd9cc69bcc193b3
Julien Buret <julien.buret@gmail.com>
2014-03-05 16:20:57 +0100

    add: new stat

--- scripts/animated-percent.js
+++ scripts/animated-percent.js
@@ -1,34 +1,86 @@
-var animatedCounter = {
+var animatedPercent = {
 
-    init: function (container, data) {
+    init: function (container) {
         return {
             container: container,
-            data: data,
-            draw: function () {
-                this.container.empty();
+            draw: function (percent, width, height, text_y) {
+                var duration = 3000,
+                    transition = 50;
+                /*
+                 drawDonutChart(
+                 '#donut',
+                 $('#donut').data('donut'),
+                 290,
+                 290,
+                 ".35em"
+                 );
+                 */
+
+                width = typeof width !== 'undefined' ? width : 250;
+                height = typeof height !== 'undefined' ? height : 250;
+                text_y = typeof text_y !== 'undefined' ? text_y :".35em";
+
+                var dataset = {
+                        lower: calcPercent(0),
+                        upper: calcPercent(percent)
+                    },
+                    radius = Math.min(width, height) / 2,
+                    pie = d3.layout.pie().sort(null),
+                    format = d3.format(".0%");
+
+                var arc = d3.svg.arc()
+                    .innerRadius(radius - 20)
+                    .outerRadius(radius);
+
+                console.log(this.container)
                 var svg = d3.select(this.container.get(0)).append("svg")
-                    .attr("width", 100)
-                    .attr("height", 100)
+                    .attr("width", width)
+                    .attr("height", height)
+                    .append("g")
+                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-                svg.selectAll(".txt")
-                    .data([data.max])
-                    .enter()
-                    .append("text")
-                    .text("")
-                    .attr("class", "counter")
-                    .attr("transform", "translate(" + 100 / 2 + "," + 100 * 0.85 + ")")
-                    .transition()
-                    .duration(data.duration)
-                    .tween("text", function (d) {
-                        var i = d3.interpolate(this.textContent, d),
-                            prec = (d + "").split("."),
-                            round = (prec.length > 1) ? Math.pow(10, prec[1].length) : 1;
+                var path = svg.selectAll("path")
+                    .data(pie(dataset.lower))
+                    .enter().append("path")
+                    .attr("class", function (d, i) {
+                        return "color" + i
+                    })
+                    .attr("d", arc)
+                    .each(function (d) {
+                        this._current = d;
+                    }); // store the initial values
 
-                        return function (t) {
-                            this.textContent = data.prefix + (Math.round(i(t) * round) / round) + data.suffix;
-                        };
-                    });
+                var text = svg.append("text")
+                    .attr("text-anchor", "middle")
+                    .attr("dy", text_y);
+
+                if (typeof(percent) === "string") {
+                    text.text(percent);
+                }
+                else {
+                    var progress = 0;
+                    var timeout = setTimeout(function () {
+                        clearTimeout(timeout);
+                        path = path.data(pie(dataset.upper)); // update the data
+                        path.transition().duration(duration).attrTween("d", function (a) {
+                            // Store the displayed angles in _current.
+                            // Then, interpolate from _current to the new angles.
+                            // During the transition, _current is updated in-place by d3.interpolate.
+                            var i = d3.interpolate(this._current, a);
+                            var i2 = d3.interpolate(progress, percent)
+                            this._current = i(0);
+                            return function (t) {
+                                text.text(format(i2(t) / 100));
+                                return arc(i(t));
+                            };
+                        }); // redraw the arcs
+                    }, 200);
+                }
             }
         }
     }
+}
+
+function calcPercent(percent) {
+    return [percent, 100 - percent];
 }
---
+++ scripts/animated-text.js
@@ -0,0 +1,34 @@
+var animatedCounter = {
+
+    init: function (container, data) {
+        return {
+            container: container,
+            data: data,
+            draw: function () {
+                this.container.empty();
+                var svg = d3.select(this.container.get(0)).append("svg")
+                    .attr("width", this.container.width())
+                    .attr("height", 100)
+
+                svg.selectAll(".txt")
+                    .data([data.max])
+                    .enter()
+                    .append("text")
+                    .text("")
+                    .attr("class", "counter")
+                    .attr("transform", "translate(" + 100 / 2 + "," + 100 * 0.85 + ")")
+                    .transition()
+                    .duration(data.duration)
+                    .tween("text", function (d) {
+                        var i = d3.interpolate(this.textContent, d),
+                            prec = (d + "").split("."),
+                            round = (prec.length > 1) ? Math.pow(10, prec[1].length) : 1;
+
+                        return function (t) {
+                            this.textContent = data.prefix + (Math.round(i(t) * round) / round) + data.suffix;
+                        };
+                    });
+            }
+        }
+    }
+}
